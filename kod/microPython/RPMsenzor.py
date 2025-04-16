e65bae9e58dddde8b3b1369e2dd85e9cb46f98a4
Vojta Danda <archimedesvojta@gmail.com>
2025-04-16 21:49:36 +0200

    • spojeni složek pro Pycharm a Thonny (upravuji ted stejny soubor) • oprava IRQ -> už správně funguje měření RPM

---
+++ kod/microPython/RPMsenzor.py
@@ -0,0 +1,57 @@
+from machine import Pin, Timer
+import time
+
+# --- Nastaveni ---
+PULSE_PIN = 2
+LED_PIN = 19
+PULSES_PER_REV = 1
+DEBOUNCE_TIME_MS = 50
+
+# --- Inicializace ---
+led = Pin(LED_PIN, Pin.OUT)
+pulse_pin = Pin(PULSE_PIN, Pin.IN, Pin.PULL_UP)
+
+# --- Promenne ---
+pulse_count = 0
+rpm = 0
+last_pulse_time = 0
+last_state = 1  # predchozi stav pinu (1 == HIGH)
+
+# --- Spolecny IRQ handler ---
+def edge_handler(pin):
+    global pulse_count, rpm, last_pulse_time, last_state
+    now = time.ticks_ms()
+    state = pin.value()
+
+    # Debounce
+    if time.ticks_diff(now, last_pulse_time) > DEBOUNCE_TIME_MS:
+        if state == 1 and last_state == 0:
+            # Nabezna hrana (inverzní logika)
+            led.value(0)
+            print("UVOLNENI")
+        elif state == 0 and last_state == 1:
+            # Sestupna hrana (inverzní logika)
+            pulse_count += 1
+            led.value(1)
+            print("PULZ DETEKOVAN")
+        last_pulse_time = now
+        last_state = state
+
+# --- Timer pro vypocet RPM ---
+def calc_rpm(timer):
+    global pulse_count, rpm
+    rps = pulse_count / PULSES_PER_REV
+    rpm = rps * 60
+    print("RPS: {:.2f}  RPM: {:.2f}".format(rps, rpm))
+    pulse_count = 0
+
+# --- Nastaveni IRQ pro obe hrany ---
+pulse_pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=edge_handler)
+
+# --- Timer ---
+timer = Timer(0)
+timer.init(period=1000, mode=Timer.PERIODIC, callback=calc_rpm)
+
+# --- Hlavni smycka ---
+while True:
+    time.sleep(10)
---
+++ kod/microPython/boot.py
@@ -0,0 +1,20 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+#import ledka_blik
+
+import machine
+import os
+
+# výpis základních informací
+print("Booting ESP32...")
+print("Filesystem obsahuje:", os.listdir())
+
+# (Volitelně) můžeš sem dát inicializaci sériového spojení nebo jiných periferií, ale není nutné
+
+from time import sleep
+sleep(5)
+#print("Starting RPMsenzor.py ...")
+#import RPMsenzor
